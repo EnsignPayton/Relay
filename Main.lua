def88a5f8cdcc82dd3cd201c143a70036d0af614
Logan Payton <Logan.Payton@javs.com>
2017-12-14 22:01:16 -0500

    Add main file with reference to AceAddon

---
+++ Main.lua
@@ -0,0 +1,14 @@
+Relay = LibStub("AceAddon-3.0"):NewAddon("Relay")
+
+function Relay:OnInitialize()
+	-- TODO: Initialization
+end
+
+function Relay:OnEnable()
+	-- TODO: Enable
+end
+
+function Relay:OnDisable()
+	-- TODO: Disable
+end
+
