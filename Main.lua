60d45af12fa4393be1e6ec4900575e434b83a2b1
Logan Payton <Logan.Payton@javs.com>
2017-12-14 22:05:01 -0500

    Enable AceConsole and use Print for hello world test

--- Main.lua
+++ Main.lua
@@ -1,7 +1,8 @@
-Relay = LibStub("AceAddon-3.0"):NewAddon("Relay")
+Relay = LibStub("AceAddon-3.0"):NewAddon("Relay", "AceConsole-3.0")
 
 function Relay:OnInitialize()
 	-- TODO: Initialization
+	Relay:Print("Hello, world")
 end
 
 function Relay:OnEnable()
