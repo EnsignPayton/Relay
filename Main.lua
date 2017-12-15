58d47fa33a4868c1f8eed04c8d44086a38741e60
Logan Payton <Logan.Payton@javs.com>
2017-12-14 23:03:26 -0500

    Add in a bunch of example junk. Remove what we dont need later

--- Main.lua
+++ Main.lua
@@ -1,15 +1,42 @@
-Relay = LibStub("AceAddon-3.0"):NewAddon("Relay", "AceConsole-3.0")
+Relay = LibStub("AceAddon-3.0"):NewAddon("Relay", 
+	"AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0")
+
+local options = {
+	name = "Relay",
+	handler = Relay,
+	type = 'group',
+	args = {
+		msg = {
+			type = 'input',
+			name = 'My Message',
+			desc = 'The message for my addon',
+			set = 'SetMyMessage',
+			get = 'GetMyMessage'
+		}
+	}
+}
+
+function Relay:GetMyMessage(info)
+	return self.db.global.myMessage
+end
+
+function Relay:SetMyMessage(info, input)
+	self.db.global.myMessage = input
+end
+
+LibStub("AceConfig-3.0"):RegisterOptionsTable("Relay", options, {"relay"})
+
+-- Lifecycle Management Functions
 
 function Relay:OnInitialize()
-	-- TODO: Initialization
-	Relay:Print("Hello, world")
+	options.args.profile = LibStub("AceDBOptions-3.0"):GetOptionsTable(db)
+	self.db = LibStub("AceDB-3.0"):New("RelayDB")
 end
 
 function Relay:OnEnable()
-	-- TODO: Enable
 end
 
 function Relay:OnDisable()
-	-- TODO: Disable
 end
+
 
