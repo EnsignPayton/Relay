9e17172fb52a5fc961f66ee2699d177a0b2330fc
Logan Payton <Logan.Payton@javs.com>
2017-12-16 23:44:24 -0500

    Fix echo, remove config for now

--- Main.lua
+++ Main.lua
@@ -3,47 +3,11 @@
 --
 
 Relay = LibStub("AceAddon-3.0"):NewAddon("Relay", 
-	"AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0")
+	"AceConsole-3.0", "AceComm-3.0")
 
-local defaults = {
-	profile = {
-		autoGrats = true,
-		echo = false,
-	},
-}
-
-local options = {
-	name = "Relay",
-	handler = Relay,
-	type = "group",
-	args = {
-		echo = {
-			name = "Echo",
-			desc = "Sends a message to be echoed over guild chat",
-			type = "execute",
-			func = "SendEcho"
-		},
-		enables = {
-			name = "Enables",
-			desc = "Feature enable toggle",
-			type = "group",
-			args = {
-				echo = {
-					name = "Echo",
-					desc = "Enables or disables message echoing",
-					type = "toggle",
-					set = function(info, val) Relay.db.profile.echo = val end,
-					get = function(info) return Relay.db.profile.echo end
-				},
-			},
-		},
-	}
-}
-
-LibStub("AceConfig-3.0"):RegisterOptionsTable("Relay", options, {"relay"})
 
 function Relay:OnInitialize()
-	self.db = LibStub("AceDB-3.0"):New("RelayDB", defaults)
+	Relay:RegisterComm("Relay")
 end
 
 function Relay:OnEnable()
@@ -53,18 +17,17 @@ function Relay:OnDisable()
 end
 
 --------------------------------------------------------------------------------
--- Echo
+-- AceComm Support
 --
-
-Relay:RegisterComm("Relay")
-
-function Relay:OnCommReceived(prefix, message, distribution, sender)
-	if (distribution ~= "GUILD") then return end
-	local result = message
-	SendChatMessage(result, distribution)
-end
 
 function Relay:SendEcho(message)
 	self:SendCommMessage("Relay", message, "GUILD")
 end
+
+function Relay:OnCommReceived(prefix, message, distribution, sender)
+	if (distribution ~= "GUILD" or sender == UnitName("player")) then return end
+	local result = message
+	SendChatMessage(result, distribution)
+end
+
 
